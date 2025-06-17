7e5cd47a9dbb2c6e6aadfb1800e28677d272ced1
Muhammad Ahmad <ahmadniazpk123@gmail.com>
2025-06-18 00:06:16 +0500

    added ladning page, setup basic server

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import HeroSection from "./heroSection";
+import FeaturesSection from "./featuresSection";
+import DiscoverSection from "./discoverSection";
+import CallToActionSection from "./callToAction";
+import FooterSection from "./footerSection";
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div>
+      <HeroSection />
+      <FeaturesSection />
+      <DiscoverSection />
+      <CallToActionSection />
+      <FooterSection />
+    </div>
+  );
 };
 
 export default Landing;
